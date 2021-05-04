5674dd40b488397b71f72ca43174c063a5092897
cghita <cghita@airtouchmedia.com>
2021-05-04 18:35:57 +0300

    Add fragment for test app

---
+++ app/src/main/java/com/codingtroops/screentracker/HelloWorldFragment.kt
@@ -0,0 +1,19 @@
+package com.codingtroops.screentracker
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class HelloWorldFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_hello_world, container, false)
+    }
+
+}
