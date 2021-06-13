721001aab1a56e504b96a28a0e9494bbb7515292
cghita <catalinghita8@gmail.com>
2021-06-13 17:05:16 +0300

    Rollback to previous fragment when DialogFragments are destroyed

--- screentracker/src/main/java/com/codingtroops/screentracker/ScreenTracker.kt
+++ screentracker/src/main/java/com/codingtroops/screentracker/ScreenTracker.kt
@@ -11,17 +11,22 @@ import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.startForegroundService
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
 import androidx.lifecycle.ProcessLifecycleOwner
+import com.codingtroops.screentracker.ScreenTracker.getClassNameWithExtension
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 
 object ScreenTracker {
 
     private lateinit var application: Application
+    private var lastFragmentClass: Class<out Any>? = null
 
     private const val ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 5469
 
@@ -79,15 +84,30 @@ object ScreenTracker {
             object : FragmentManager.FragmentLifecycleCallbacks() {
                 override fun onFragmentResumed(fm: FragmentManager, f: Fragment) {
                     super.onFragmentResumed(fm, f)
-                    if (!isClassExcluded(f.javaClass))
-                        TextOverlayService.setText(
-                            application,
-                            activity.javaClass.getClassNameWithExtension(),
-                            f.javaClass.getClassNameWithExtension()
-                        )
+                    if (!isClassExcluded(f.javaClass)) {
+                        sendScreenDetails(activity.javaClass, f.javaClass)
+                        if (!f.isDialog())
+                            lastFragmentClass = f.javaClass
+                    }
+                }
+
+                override fun onFragmentViewDestroyed(fm: FragmentManager, f: Fragment) {
+                    super.onFragmentViewDestroyed(fm, f)
+                    val lastFragment = lastFragmentClass
+                    // If a Dialog Fragment is destroyed, we must rollback to the previous fragment
+                    if (f.isDialog() && lastFragment != null)
+                        sendScreenDetails(activity.javaClass, lastFragment.javaClass)
                 }
             },
             true
+        )
+    }
+
+    private fun sendScreenDetails(activityClass: Class<out Any>, fragmentClass: Class<out Any>) {
+        TextOverlayService.setText(
+            application,
+            activityClass.getClassNameWithExtension(),
+            fragmentClass.getClassNameWithExtension()
         )
     }
 
@@ -99,6 +119,9 @@ object ScreenTracker {
             this.simpleName + ".kt"
         else this.simpleName + ".java"
     }
+
+    private fun Fragment.isDialog() =
+        (this is BottomSheetDialogFragment || this is DialogFragment || this is BottomSheetDialog)
 
     private fun Class<out Any>.isKotlin() =
         this.declaredAnnotations.any { it.annotationClass == Metadata::class }
